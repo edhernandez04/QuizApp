d16c7c39655602fdb8511b71fbae3af015c01d32
edhernandez04 <mchh.004@gmail.com>
2020-08-19 15:32:15 -0400

    navigation established

--- App/index.js
+++ App/index.js
@@ -1,4 +1,20 @@
-import Quiz from'./screens/Quiz'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createAppContainer } from '@react-navigation/native'
 
+import QuizIndex from './screens/QuizIndex'
+import Quiz from './screens/Quiz'
 
-export default Quiz
+const Stack = createStackNavigator()
+
+const MainStack = () => (
+  <NavigationContainer>
+  <Stack.Navigator>
+    <Stack.Screen name="Quizzes" component={QuizIndex} />
+    <Stack.Screen name="Quiz" component={Quiz} />
+  </Stack.Navigator>
+</NavigationContainer>
+)
+
+export default MainStack
